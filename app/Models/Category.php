c942db0d76ffcedecf6af09411f7d5159fced49e
robbytrujillo <robbyikusuma88@gmail.com>
2025-10-18 11:20:20 +0700

    Introduction Buy Tickets

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
@@ -17,4 +18,8 @@ class Category extends Model
         'slug',
         'icon'
     ];
+
+    public function tickets(): HasMany {
+        return $this->hasMany(Ticket::class);
+    }
 }
