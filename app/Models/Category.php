9eb99b6feca107189b2efdc94016a6437f237593
robbytrujillo <robbyikusuma88@gmail.com>
2025-10-18 13:24:14 +0700

    Introduction Buy Tickets

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -18,6 +19,11 @@ class Category extends Model
         'slug',
         'icon'
     ];
+
+    public function setNameAttribute($value) {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 
     public function tickets(): HasMany {
         return $this->hasMany(Ticket::class);
