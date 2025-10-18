0fd1199fc4f0b55f5ad6cca5b49d48d1b6849d34
robbytrujillo <robbyikusuma88@gmail.com>
2025-10-18 10:33:18 +0700

    Introduction Buy Tickets

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
     //
+    use HasFactory, SoftDeletes;
+
+    // mass assigment
+    protected $fillable = [
+        'name',
+        'slug',
+        'icon'
+    ];
 }
--- app/Models/TicketPhoto.php
+++ app/Models/TicketPhoto.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TicketPhoto extends Model
 {
     //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'photo',
+        'ticket_id',
+    ];
 }
