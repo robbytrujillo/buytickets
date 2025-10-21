0a1a75f21fcd6740d874baaeb40106171bc31c7c
robbytrujillo <robbyikusuma88@gmail.com>
2025-10-21 10:26:39 +0700

    Create Repository Data Tickets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\TicketRepositoryInterface;
+use App\Repositories\TicketRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->singleton(TicketRepositoryInterface::class, TicketRepository::class);
     }
 
     /**
