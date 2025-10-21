66e57479616fe9938fede314717131a9963ad888
robbytrujillo <robbyikusuma88@gmail.com>
2025-10-21 09:24:33 +0700

    Introduction Buy Tickets | Run Service Repository Pattern

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     //
+    protected $frontService;
+
+    public function __construct(FrontService $frontService) {
+        $this->frontService = $frontService;
+    }
+
+    // konsep service repository pattern
+    public function index() {
+        $data = $this->frontService->getFrontPageData();
+        return view('front.index', $data);
+    }
+
+    // konsep MVC
+    // public function index2() {
+    //     $categories = Category::latest()->get();
+    //     $popular_tickets = Ticket::where('is_popular', true)->take(4)->get();
+    //     $new_tickets = Ticket::latest()->get();
+    //     return view('front.index', compant('categories', 'popular_tickets', 'new_tickets'));
+    // }
+
 }
