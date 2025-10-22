29d7efc960af453ad87631735d8a43fd25d097c9
robbytrujillo <robbyikusuma88@gmail.com>
2025-10-23 05:38:11 +0700

    Create code booking controller

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
+use App\Services\BookingService;
 
 class BookingController extends Controller
 {
     //
+    protected $bookingService;
+
+    public function __construct(BookingService $bookingService) {
+        $this->bookingService = $bookingService;
+    }
+
+    public function booking(Ticket $ticket) {
+        return view('front.booking', compact($ticket));
+    }
 }
